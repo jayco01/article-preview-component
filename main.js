1a69dcee46d1451df9724f3486a1f87fa8eb8f8b
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-03-02 22:02:13 -0700

    wrote the active state of the share button in javascript

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const shareBtn = document.querySelector(".social__arrow");
+const active = document.querySelector(".social__popup");
+const activeBtn = document.querySelector(".social__arrow--active");
+
+shareBtn.addEventListener("click", () => {
+    active.classList.toggle('active-state');
+});
+
+activeBtn.addEventListener("click", () => {
+    active.classList.toggle('active-state');
+});
